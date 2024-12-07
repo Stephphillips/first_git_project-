5f72cc227e9bcde1131616efaae64707a4fb97ed
Stephanie <Step.91phillips@gmail.com>
2024-12-07 18:56:52 -0500

    HelloCydeo Updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,6 +3,14 @@ package day01;
 import org.w3c.dom.ls.LSOutput;
 
 public class HelloCydeo {
-  }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+
+    }
+
+
+
 
 }
